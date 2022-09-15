d4be623e0516f0d367050b5378c5f0b37bc73e44
Lucas C. Pauli <lucaspauli22@gmail.com>
2022-09-15 20:47:03 -0300

    FundoMobile refatorado com map e const dentro

--- src/Components/FundoMobile.js
+++ src/Components/FundoMobile.js
@@ -1,16 +1,16 @@
-const ionIcons = [
-    {name: "home"},
-    {name: "search-outline"},
-    {name: "add-circle-outline"},
-    {name: "heart-outline"},
-    {name: "person-outline"}
-];
-
 export default function FundoMobile() {
-    return (
-        <div>
-            {ionIcons.map((a) => <ion-icon name={a.name}></ion-icon>)}
-        </div>
-    )
-}
-
+  const ionIcons = [
+    { name: "home" },
+    { name: "search-outline" },
+    { name: "add-circle-outline" },
+    { name: "heart-outline" },
+    { name: "person-outline" },
+  ];
+  return (
+    <div>
+      {ionIcons.map((a) => (
+        <ion-icon name={a.name}></ion-icon>
+      ))}
+    </div>
+  );
+};
