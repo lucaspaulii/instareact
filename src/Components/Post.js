5fdfdafa8b3bea8d83845c806042d3b728880b71
Lucas C. Pauli <lucaspauli22@gmail.com>
2022-09-17 01:43:07 -0300

    like feature added

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,13 +1,27 @@
 import React from "react";
 export default function Post(props) {
+  const [likeCount, setLikeCount] = React.useState(props.curtidasQuantidade)
+  const [isLiked, setIsLiked] = React.useState(false)
+
+  function like() {
+    if (isLiked === false) {
+      setLikeCount(likeCount + 1);
+      setIsLiked(true)
+    } else {
+      setLikeCount(likeCount - 1);
+      setIsLiked(false)
+    }
+  }
   return (
     <div class="post">
       <TopoPost userImg={props.userImg} userText={props.userText} />
-      <ConteudoPost conteudoImg={props.conteudoImg} />
+      <ConteudoPost conteudoImg={props.conteudoImg} handleLike={() => like()} />
       <FundoPost
         curtidasImg={props.curtidasImg}
         curtidasTexto={props.curtidasTexto}
-        curtidasQuantidade={props.curtidasQuantidade}
+        curtidasQuantidade={likeCount}
+        handleLike={() => like()}
+        isLiked={isLiked}
       />
     </div>
   );
@@ -30,15 +44,16 @@ function TopoPost(props) {
 function ConteudoPost(props) {
   return (
     <div class="conteudo">
-      <img src={props.conteudoImg} />
+      <img src={props.conteudoImg} onClick={props.handleLike}/>
     </div>
   );
 }
 
 function FundoPost(props) {
+
   return (
     <div class="fundo">
-      <Acoes />
+      <Acoes handleLike={props.handleLike} isLiked={props.isLiked} />
       <Curtidas
         curtidasImg={props.curtidasImg}
         curtidasTexto={props.curtidasTexto}
@@ -48,7 +63,7 @@ function FundoPost(props) {
   );
 }
 
-function Acoes() {
+function Acoes(props) {
   const [isClicked, setIsClicked] = React.useState(false)
   function handleSave() {
     let newClick = (isClicked ? false : true)
@@ -57,7 +72,7 @@ function Acoes() {
   return (
     <div class="acoes">
       <div>
-        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon name={props.isLiked ? "heart" : "heart-outline"} class={(props.isLiked) && ('red')} onClick={props.handleLike}></ion-icon>
         <ion-icon name="chatbubble-outline"></ion-icon>
         <ion-icon name="paper-plane-outline"></ion-icon>
       </div>
